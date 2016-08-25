c1e95923f7f9f9840d1b84ed47a1d0bbda92c010
Allison Padget <allisonpadget@macbook-pro.gateway.pace.com>
2016-08-24 18:25:07 -0600

    fixed highlighting issue
    when skipping around video, highlighting wouldn’t remove on some lines
    - fixed

--- js/app.js
+++ js/app.js
@@ -141,58 +141,157 @@ function endHighlight(endTime) {
 }
 
 
+//removes highlighting properly when skipping around
+
 video.addEventListener('timeupdate', function(){
   var time = video.currentTime;
 
   if (time > 0.24 && time < 4.13) {
       startHighlight('00.24');
-  } else if (time >= 4.13 && time < 7.535) {
+  } else {
       endHighlight('00.24');
-      startHighlight('4.13'); 
-  } else if (time >= 7.535 && time < 11.27) {
+  }
+  if (time >= 4.13 && time < 7.535) {
+      startHighlight('4.13')
+  } else {
       endHighlight('4.13');
-      startHighlight('7.535');
-  } else if (time >= 11.27 && time < 13.96) {
-      endHighlight('7.535');
-      startHighlight('11.27');
-  } else if (time >= 13.96 && time < 17.94) {
-      endHighlight('11.27');
-      startHighlight('13.96');
-  } else if (time >= 17.94 && time < 22.37) {
-      endHighlight('13.96');
-      startHighlight('17.94');
-  } else if (time >= 22.37 && time < 26.88) {
-      endHighlight('17.94');
-      startHighlight('22.37');
-  } else if (time >= 26.88 && time < 32.1) {
-      endHighlight('22.37');
-      startHighlight('26.88');
-  } else if (time >= 32.1 && time < 34.73) {
-      endHighlight('26.88');
-      startHighlight('32.1');
-  } else if (time >= 34.73 && time < 39.43) {
-      endHighlight('32.1');
-      startHighlight('34.73');
-  } else if (time >= 39.43 && time < 42.35) {
-      endHighlight('34.73');
-      startHighlight('39.43');
-  } else if (time >= 42.35 && time < 46.3) {
-      endHighlight('39.43');
-      startHighlight('42.35');
-  } else if (time >= 46.3 && time < 49.27) {
-      endHighlight('42.35');
-      startHighlight('46.3');
-  } else if (time >= 49.27 && time < 53.76) {
-      endHighlight('46.3');
-      startHighlight('49.27');
-  } else if (time >= 53.76 && time < 57.78) {
-      endHighlight('49.27');
-      startHighlight('53.76');
-  } else if (time >= 57.78 && time < 59.00) {
-      endHighlight('53.76');
-      startHighlight('57.78');
+  }
+  if (time >= 7.535 && time < 11.27){
+    startHighlight('7.535');
+  } else {
+     endHighlight('7.535');
+  }
+  if (time >= 11.27 && time < 13.96){
+    startHighlight('11.27');
+  } else {
+     endHighlight('11.27');
+  }
+  if (time >= 13.96 && time < 17.94) {
+    startHighlight('13.96');
+  } else {
+    endHighlight('13.96');
+  }
+  if (time >= 17.94 && time < 22.37) {
+    startHighlight('17.94');
+  } else {
+    endHighlight('17.94');
+  }
+  if (time >= 22.37 && time < 26.88) {
+    startHighlight('22.37');
+  } else {
+    endHighlight('22.37');
+  }
+  if (time >= 26.88 && time < 32.1) {
+    startHighlight('26.88');
+  } else {
+    endHighlight('26.88')
+  }
+  if (time >= 32.1 && time < 34.73) {
+    startHighlight('32.1');
+  } else {
+    endHighlight('32.1');
+  }
+  if (time >= 34.73 && time < 39.43) {
+    startHighlight('34.73');
+  } else {
+    endHighlight('34.73');
+  }
+  if (time >= 39.43 && time < 42.35) {
+    startHighlight('39.43');
+  } else {
+    endHighlight('39.43');
+  }
+  if (time >= 42.35 && time < 46.3) {
+    startHighlight('42.35');
+  } else {
+    endHighlight('42.35');
+  }
+  if (time >= 46.3 && time < 49.27) {
+    startHighlight('46.3');
+  } else {
+    endHighlight('46.3');
+  }
+  if (time >= 49.27 && time < 53.76) {
+    startHighlight('49.27');
+  } else {
+    endHighlight('49.27');
+  }
+  if (time >= 53.76 && time < 57.78) {
+    startHighlight('53.76');
+  } else {
+    endHighlight('53.76');
+  }
+  if (time >= 57.78 && time < 59.00) {
+    startHighlight('57.78');
+  } else {
+    endHighlight('57.78');
   }
 });
+
+
+
+
+
+
+
+
+
+
+//old
+
+
+// video.addEventListener('timeupdate', function(){
+//   var time = video.currentTime;
+
+//   if (time > 0.24 && time < 4.13) {
+//       startHighlight('00.24');
+//   } else if (time >= 4.13 && time < 7.535) {
+//       endHighlight('00.24');
+//       startHighlight('4.13'); 
+//   } else if (time >= 7.535 && time < 11.27) {
+//       endHighlight('4.13');
+//       startHighlight('7.535');
+//   } else if (time >= 11.27 && time < 13.96) {
+//       endHighlight('7.535');
+//       startHighlight('11.27');
+//   } else if (time >= 13.96 && time < 17.94) {
+//       endHighlight('11.27');
+//       startHighlight('13.96');
+//   } else if (time >= 17.94 && time < 22.37) {
+//       endHighlight('13.96');
+//       startHighlight('17.94');
+//   } else if (time >= 22.37 && time < 26.88) {
+//       endHighlight('17.94');
+//       startHighlight('22.37');
+//   } else if (time >= 26.88 && time < 32.1) {
+//       endHighlight('22.37');
+//       startHighlight('26.88');
+//   } else if (time >= 32.1 && time < 34.73) {
+//       endHighlight('26.88');
+//       startHighlight('32.1');
+//   } else if (time >= 34.73 && time < 39.43) {
+//       endHighlight('32.1');
+//       startHighlight('34.73');
+//   } else if (time >= 39.43 && time < 42.35) {
+//       endHighlight('34.73');
+//       startHighlight('39.43');
+//   } else if (time >= 42.35 && time < 46.3) {
+//       endHighlight('39.43');
+//       startHighlight('42.35');
+//   } else if (time >= 46.3 && time < 49.27) {
+//       endHighlight('42.35');
+//       startHighlight('46.3');
+//   } else if (time >= 49.27 && time < 53.76) {
+//       endHighlight('46.3');
+//       startHighlight('49.27');
+//   } else if (time >= 53.76 && time < 57.78) {
+//       endHighlight('49.27');
+//       startHighlight('53.76');
+//   } else if (time >= 57.78 && time < 59.00) {
+//       endHighlight('53.76');
+//       startHighlight('57.78');
+//   }
+// });
 
 
 
